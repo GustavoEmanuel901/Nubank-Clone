3cfd3fe2b7600c0882ce5f3099221f569afad2da
Gustavo Emanuel <gustavoemanuel901@gmail.com>
2020-08-29 15:40:37 -0300

    Card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Header from '~/components/Header'
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles'
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation } from './styles'
 
 export default function Main() {
   return (
     <Container>
       <Header/>
+
+      <Content>
+        <Card>
+          <CardHeader>
+              <Icon name='attach-money' size={28} color='#666'/>
+              <Icon name='visibility-off' size={28} color='#666'/>
+          </CardHeader>
+          <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 76.500,00</Description>
+          </CardContent>
+          <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Lusdete Ornelas as 06:00h
+              </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs/>
     </Container>
   )
